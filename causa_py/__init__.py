27787c4e7c6dd828ae1c9bad4026e3ad4945a9cf
ታዲዮስ || Tadiyos Aschalew <nexuss0781@gmail.com>
2025-07-29 20:23:26 +0300

    update causa_py/__init__.py

--- causa_py/__init__.py
+++ causa_py/__init__.py
@@ -2,7 +2,6 @@ import importlib.util
 import sys
 import os
 
-# Path to Rust compiled shared object
 so_path = os.path.join(os.path.dirname(__file__), "causa_py.so")
 
 spec = importlib.util.spec_from_file_location("causa_py", so_path)
@@ -10,6 +9,7 @@ causa_py = importlib.util.module_from_spec(spec)
 sys.modules["causa_py"] = causa_py
 spec.loader.exec_module(causa_py)
 
-# Expose Rust structs/classes to Python package
 Manifold = causa_py.Manifold
 Event = causa_py.Event
+
+from . import physics
